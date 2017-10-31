0a2b26fcced358ce286ec95987e221f8e0a6a29c
Joel Chan <joelchan1220@gmail.com>
2017-10-30 18:22:56 -0600

    master

---
+++ Proyecto/Proyecto/Controllers/todo_proController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.Controllers
+{
+    public class todo_proController : Controller
+    {
+        // GET: todo_pro
+        public ActionResult inicio()
+        {
+            return View();
+        }
+    }
+}
