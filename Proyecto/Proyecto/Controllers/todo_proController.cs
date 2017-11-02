896ad685d2d2138c01fac74151596fd432601dc8
Joel Chan <joelchan1220@gmail.com>
2017-11-01 20:01:49 -0600

    login

--- Proyecto/Proyecto/Controllers/todo_proController.cs
+++ Proyecto/Proyecto/Controllers/todo_proController.cs
@@ -3,15 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Proyecto.BO;
+using Proyecto.DAO;
 
 namespace Proyecto.Controllers
 {
     public class todo_proController : Controller
     {
         // GET: todo_pro
+        loginDAO log = new loginDAO();
         public ActionResult inicio()
         {
             return View();
+        }
+        public ActionResult vacio1()
+        {
+            return View();
+        }
+        public ActionResult vacio()
+        {
+            // return View(id==""  ? new usuarioBO() :log.obtener(id));
+            return View();
+        }
+        public ActionResult login(usuarioBO usuario)
+        {
+            if (log.verificar(usuario))
+            {
+                var i = log.buscarelid(usuario);
+                Session["id_tipo"] = i;
+                Response.Write("<script>alert('usuario correcto')</script>");
+
+
+                if (i == 1)
+                {
+              return  Redirect("~/todo_pro/inicio");
+
+
+                }
+                else if (i == 2)
+                {
+              return     Redirect("~/todo_pro/inicio");
+                }
+                else if (i == 3)
+                {
+
+           return   Redirect("~/todo_pro/inicio");
+                }
+                
+            }
+           
+        return Redirect("~/todo_pro/vacio");
+            
+
+            
+      
         }
     }
 }
--- Proyecto/Proyecto/DAO/loginDAO.cs
+++ Proyecto/Proyecto/DAO/loginDAO.cs
@@ -13,7 +13,7 @@ namespace Proyecto.DAO
         {
             usuarioBO usuario = (usuarioBO)agregar;
             Conexion_DAOcomant conectar = new Conexion_DAOcomant();
-            SqlCommand cmd = new SqlCommand("select count(usuario) CODIGO from usuarios where usuario=@usuario and contraseña=@contra");
+            SqlCommand cmd = new SqlCommand("select count(nombre) ID from usuario where nombre=@usuario and contraseña=@contra");
             cmd.Parameters.Add("@usuario", SqlDbType.VarChar).Value = usuario.nombre;
             cmd.Parameters.Add("@contra", SqlDbType.VarChar).Value = usuario.contraseña;
             cmd.CommandType = CommandType.Text;
@@ -23,6 +23,33 @@ namespace Proyecto.DAO
 
 
 
+        }
+        public int buscarelid(object agregar)
+        {
+            usuarioBO usuario = (usuarioBO)agregar;
+            Conexion_DAOcomant conectar = new Conexion_DAOcomant();
+            SqlCommand cmd = new SqlCommand("select ID_tipo, nombre from usuario where	Nombre=@usuario and contraseña=@contra");
+            cmd.Parameters.Add("@usuario", SqlDbType.VarChar).Value = usuario.nombre;
+            cmd.Parameters.Add("@contra", SqlDbType.VarChar).Value = usuario.contraseña;
+            cmd.CommandType = CommandType.Text;
+            int resultado = conectar.EjecutarComando(cmd);
+
+            return resultado;
+
+
+
+        }
+        public usuarioBO obtener(string id)
+        {
+            ConexionDAO conexion = new ConexionDAO();
+            var usuario = new usuarioBO();
+            string strbuscar = string.Format("select ID_tipo, nombre from usuario where	Nombre='"+id+"'");
+            DataTable datos = conexion.ejercutarsentrenciasdatable(strbuscar);
+            DataRow row = datos.Rows[0];
+            usuario.id = Convert.ToInt32(row["id"]);
+            usuario.nombre = row["nombre"].ToString();
+           
+            return usuario;
         }
     }
 }
