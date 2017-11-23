e6d9a1f2743061831184a3c6241c0e607085bd85
ADAN- <ADAN-@ADAN--PC>
2017-11-22 20:28:09 -0600

    clase joel

---
+++ Proyecto/Proyecto/BO/parajoelcls.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.BO
+{
+    public class parajoelcls
+    {public int id { get; set; }
+    }
+}
