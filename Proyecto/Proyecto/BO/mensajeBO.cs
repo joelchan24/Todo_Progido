838b5e4cb1ebd2f82e965daee1ec7eec5fb0c614
Joel Chan <joelchan1220@gmail.com>
2017-11-28 22:00:45 -0600

    cambios

---
+++ Proyecto/Proyecto/BO/mensajeBO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.BO
+{
+    public class mensajeBO
+    {
+        public int ID { get; set; }
+        public string mensaje { get; set; }
+        public int id_remitente { get; set; }
+        public int id_destinatario { get; set; }
+        public int status { get; set; }
+    }
+}
