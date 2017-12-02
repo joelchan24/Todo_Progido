84568672d1e3087d834e4a471df1d96eefd409ea
ADAN- <ADAN-@ADAN--PC>
2017-12-01 21:11:41 -0600

    .

--- Proyecto/Proyecto/DAO/ConexionDAO.cs
+++ Proyecto/Proyecto/DAO/ConexionDAO.cs
@@ -21,8 +21,8 @@ namespace Proyecto.DAO
 
         public SqlConnection establecerConexion()
         {
-            string cs = "Data Source=DESKTOP-TT12AGM; Initial catalog=ProyectoSOS;  integrated security=true";
-            //string cs = "Data Source=adan--pc; Initial catalog=ProyectoSOS;  integrated security=true";
+            //string cs = "Data Source=DESKTOP-TT12AGM; Initial catalog=ProyectoSOS;  integrated security=true";
+            string cs = "Data Source=adan--pc; Initial catalog=ProyectoSOS;  integrated security=true";
             //string cs = "Data Source=KAREN\\SQLEXPRESS; Initial catalog=ProyectoSOS;  integrated security=true";
 
             coneccion = new SqlConnection(cs);
