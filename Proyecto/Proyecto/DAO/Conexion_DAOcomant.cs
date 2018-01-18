66ce9c9efd48170e679b6c64027bce5bb04b8a99
Joel Chan <joelchan1220@gmail.com>
2018-01-17 18:06:59 -0600

    hjjj

--- Proyecto/Proyecto/DAO/Conexion_DAOcomant.cs
+++ Proyecto/Proyecto/DAO/Conexion_DAOcomant.cs
@@ -28,7 +28,7 @@ namespace Proyecto.DAO
         {
             //string cs = "Data Source=KAREN\\SQLEXPRESS; Initial catalog=ProyectoSOS;  integrated security=true";
             // string cs = "Data Source=DESKTOP-TT12AGM; Initial catalog=ProyectoSOS;  integrated security=true";
-            string cs = "Data Source=KAREN\\SQLEXPRESS; Initial catalog=ProyectoSOS;  integrated security=true";
+            string cs = "Data Source=DESKTOP-TT12AGM\\SQLEXPRESS; Initial catalog=ProyectoSOS;  integrated security=true";
             //string cs = "Data Source=adan--pc; Initial catalog=ProyectoSOS;  integrated security=true";
 
             coneccion = new SqlConnection(cs);
